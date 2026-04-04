4b59d877274d4a579f5e9d7deb49b7deb7a95764
João Cuculo <jv.cuculo@gmail.com>
2026-04-04 15:11:52 -0300

    initial commit

---
+++ src/main/java/com/joaocuculo/letterbooks/LetterbooksApplication.java
@@ -0,0 +1,13 @@
+package com.joaocuculo.letterbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetterbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetterbooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaocuculo/letterbooks/LetterbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaocuculo.letterbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetterbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
