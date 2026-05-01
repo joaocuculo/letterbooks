47b5918a41a0c9233298c2dbbe8f9f5f27fa5b71
João Cuculo <jv.cuculo@gmail.com>
2026-05-01 10:06:07 -0300

    feat(dto): nested dtos in one file

--- src/main/java/com/joaocuculo/letterbooks/dto/external/GoogleBooksResponseDTO.java
+++ src/main/java/com/joaocuculo/letterbooks/dto/external/GoogleBooksResponseDTO.java
@@ -8,6 +8,37 @@ import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record GoogleBooksResponseDTO(
         @JsonProperty("id") String googleBooksId,
-        @JsonProperty("volumeInfo") VolumeInfoDTO volumeInfo
+        @JsonProperty("volumeInfo") VolumeInfo volumeInfo
 ) {
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record VolumeInfo(
+            @JsonProperty("title") String title,
+            @JsonProperty("subtitle") String subtitle,
+            @JsonProperty("authors") List<String> authors,
+            @JsonProperty("publisher") String publisher,
+            @JsonProperty("publishedDate") String publishedDate,
+            @JsonProperty("description") String description,
+            @JsonProperty("industryIdentifiers") List<IndustryIdentifiers> industryIdentifiers,
+            @JsonProperty("pageCount") Integer pageCount,
+            @JsonProperty("categories") List<String> categories,
+            @JsonProperty("maturityRating") String maturityRating,
+            @JsonProperty("imageLinks") ImageLinks imageLinks,
+            @JsonProperty("language") String language
+    ) {
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record IndustryIdentifiers(
+            @JsonProperty("type") String type,
+            @JsonProperty("identifier") String identifier
+    ) {
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record ImageLinks(
+            @JsonProperty("extraLarge") String imageUrl,
+            @JsonProperty("thumbnail") String thumbnailUrl
+    ) {
+    }
 }
