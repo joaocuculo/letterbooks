2a75269fbc834ad1f3026245cd6d0fd5d6161b64
João Cuculo <jv.cuculo@gmail.com>
2026-06-18 23:28:01 -0300

    feat(category): start to implement normalization methods

--- src/main/java/com/joaocuculo/letterbooks/services/CategoryService.java
+++ src/main/java/com/joaocuculo/letterbooks/services/CategoryService.java
@@ -1,7 +1,12 @@
 package com.joaocuculo.letterbooks.services;
 
+import com.joaocuculo.letterbooks.entities.Category;
 import com.joaocuculo.letterbooks.repositories.CategoryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 @Service
 public class CategoryService {
@@ -10,5 +15,30 @@ public class CategoryService {
 
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
+    }
+
+    public Set<Category> resolveCategories(List<String> rawCategories) {
+        if (rawCategories == null || rawCategories.isEmpty()) {
+            return Set.of();
+        }
+        return Set.of(); // só para não gritar erro
+    }
+
+    private Category findOrCreateCategory(String name) {
+        return categoryRepository.findByName(name)
+                .orElseGet(() -> categoryRepository.save(new Category(name)));
+    }
+
+    private String normalizeDisplayName(String rawDisplayName) {
+        String normalizedDisplayName = rawDisplayName.trim();
+        if (normalizedDisplayName.contains("/")) {
+            List<String> cleanDisplayName = Arrays.stream(normalizedDisplayName.split("/"))
+                    .map(String::trim)
+                    .toList();
+
+            return "TEM A POSSIBILIDADE DE RETORNAR UMA LISTA DE STRING OU UMA LISTA DE STRING";
+        }
+
+        return normalizedDisplayName;
     }
 }
