e45a89896fc23e9d80d18aa7d27148d7247ee541
João Cuculo <jv.cuculo@gmail.com>
2026-07-08 20:09:32 -0300

    fix(dto): fix validation and messages

--- src/main/java/com/joaocuculo/letterbooks/dto/request/RatingRequestDTO.java
+++ src/main/java/com/joaocuculo/letterbooks/dto/request/RatingRequestDTO.java
@@ -1,12 +1,19 @@
 package com.joaocuculo.letterbooks.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record RatingRequestDTO(
-        @NotBlank(message = "Pontuação é obrigatório.") Integer score,
+        @NotNull(message = "Pontuação é obrigatório.")
+        @Min(value = 0, message = "A pontuação não pode ser menor que 0.")
+        @Max(value = 5, message = "A pontuação não pode ser maior que 5.")
+        Integer score,
         String comment,
-        @NotNull Long userId,
-        @NotNull String googleBooksId
+        @NotNull(message = "ID do usuário é obrigatório.")
+        Long userId,
+        @NotBlank(message = "ID do livro no Google Books é obrigatório.")
+        String googleBooksId
 ) {
 }
--- src/main/java/com/joaocuculo/letterbooks/dto/request/RatingUpdateDTO.java
+++ src/main/java/com/joaocuculo/letterbooks/dto/request/RatingUpdateDTO.java
@@ -1,9 +1,14 @@
 package com.joaocuculo.letterbooks.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 public record RatingUpdateDTO(
-        @NotBlank(message = "Pontuação é obrigatório.") Integer score,
+        @NotNull(message = "Pontuação é obrigatório.")
+        @Min(value = 0, message = "A pontuação não pode ser menor que 0.")
+        @Max(value = 5, message = "A pontuação não pode ser maior que 5.")
+        Integer score,
         String comment
 ) {
 }
