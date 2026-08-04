8299f27de139f6e51aac966867de66daacaa00d7
João Cuculo <jv.cuculo@gmail.com>
2026-08-04 19:26:26 -0300

    chore: permit all books endpoints and create application-dev.example.properties

--- src/main/java/com/joaocuculo/letterbooks/config/SecurityConfig.java
+++ src/main/java/com/joaocuculo/letterbooks/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/forgot-password").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/reset-password").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/books/*").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
